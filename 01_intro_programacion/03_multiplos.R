a0ada3223e15f5dae63c68549e435091d3aabb96
ismael.jimenez <ismael.jimenez@bluetab.net>
2017-11-10 23:28:13 +0100

    03_multiples fix input and refactor

--- 01_intro_programacion/03_multiplos.R
+++ 01_intro_programacion/03_multiplos.R
@@ -1,4 +1,11 @@
-input <- 1:1000
-multiples <- input[(input %% 3) == 0 | (input %% 5) == 0]
-suma <- sum(multiples)
+# Vector from 1 to 999
+input <- 1:999
 
+#Vector with multiples of 3 or 5
+multiples <- input[(input %% 3) == 0 | (input %% 5) == 0]
+
+#Sum of components of the vector
+sum_multiples <- sum(multiples)
+
+#Print the result
+sum_multiples
