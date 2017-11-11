f09d3d466d9cc44a8897e002cb855ab574e9eab8
ismael.jimenez <ismael.jimenez@bluetab.net>
2017-11-11 19:09:33 +0100

    Improved code

--- 01_intro_programacion/03_multiplos.R
+++ 01_intro_programacion/03_multiplos.R
@@ -8,4 +8,4 @@ multiples <- input[(input %% 3) == 0 | (input %% 5) == 0]
 sum_multiples <- sum(multiples)
 
 #Print the result
-paste("The sum of multiples of 3 or 5 which are less than 1000 is", sum_multiples, sept=" ")
+paste("The sum of multiples of 3 or 5 which are less than 1000 is", sum_multiples)
