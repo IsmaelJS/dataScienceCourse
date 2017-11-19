830769f5e49226841887da2b26dae619c1a89f09
ismael.jimenez <ismael.jimenez@bluetab.net>
2017-11-19 18:22:21 +0100

    02_crimenes improved

--- 02_consulta_apis/02_crimenes.R
+++ 02_consulta_apis/02_crimenes.R
@@ -1,19 +1,21 @@
-# Get the address from the next latitude and longitude
-r <- GET("http://nominatim.openstreetmap.org/reverse?", query= list(format = "json", lat= "51.4965946", lon="-0.1436476"))
+### Get the address from (lat= "51.4965946", lon="-0.1436476")
+r_address <- GET("http://nominatim.openstreetmap.org/reverse?", query= list(format = "json", lat= "51.4965946", lon="-0.1436476"))
 address <- content(r)$address
 
-#Get number of crimes commited in that location in April 2017
+### Get number of crimes commited in that location in April 2017
 r_crimes <- GET("https://data.police.uk/api/crimes-at-location", query= list(date = "2017-04",  lat = "51.4965946", lng="-0.1436476") )
 list_crimes <- content(r_crimes)
 paste(length(list_crimes), "crimes were commited in April 2017") 
 
-#Count crimes by category
-category_names <- GET("https://data.police.uk/api/crime-categories", query= list(date = "2017-04"))
-category_names_list <- content(category_names)
-names(category_names_list) <- lapply(category_names_list, function(x){x$url})
+### Count crimes by category
 
-crimes_by_category <- vapply(list_crimes, '[[', "category", FUN.VALUE = character(1))
+#Get category names
+category_names_r <- GET("https://data.police.uk/api/crime-categories", query = list(date = "2017-04"))
+category_names <- content(category_names)
+names(category_names) <- lapply(category_names, function(x){x$url})
 
+#Get crimes by category
+crimes_by_category <- vapply(list_crimes, function(crime){category_names[[crime$category]]$name}, FUN.VALUE = character(1))
 
-table(vapply(crimes_by_category, function(x){category_names_list[[x]]$name}, FUN.VALUE = character(1)))
-
+#Show table with number of crimes of each category
+table(crimes_by_category)
