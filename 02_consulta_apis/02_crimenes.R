033a5a11f7a3edde8f81806a951a0aa3e14fc4b9
ismael.jimenez <ismael.jimenez@bluetab.net>
2017-11-20 20:29:16 +0100

    fix variable names

--- 02_consulta_apis/02_crimenes.R
+++ 02_consulta_apis/02_crimenes.R
@@ -1,6 +1,6 @@
 ### Get the address from (lat= "51.4965946", lon="-0.1436476")
 r_address <- GET("http://nominatim.openstreetmap.org/reverse?", query= list(format = "json", lat= "51.4965946", lon="-0.1436476"))
-address <- content(r)$address
+address <- content(r_address)$address
 
 ### Get number of crimes commited in that location in April 2017
 r_crimes <- GET("https://data.police.uk/api/crimes-at-location", query= list(date = "2017-04",  lat = "51.4965946", lng="-0.1436476") )
@@ -11,11 +11,11 @@ paste(length(list_crimes), "crimes were commited in April 2017")
 
 #Get category names
 category_names_r <- GET("https://data.police.uk/api/crime-categories", query = list(date = "2017-04"))
-category_names <- content(category_names)
+category_names <- content(category_names_r)
 names(category_names) <- lapply(category_names, function(x){x$url})
 
 #Get crimes by category
 crimes_by_category <- vapply(list_crimes, function(crime){category_names[[crime$category]]$name}, FUN.VALUE = character(1))
 
-#Show table with number of crimes of each category
+#Show table of frequencies with number of crimes of each category
 table(crimes_by_category)
