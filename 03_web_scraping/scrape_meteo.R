028ea438837f9608704c3cab3beb72cc83a86f20
ismael.jimenez <ismael.jimenez@bluetab.net>
2017-11-27 19:23:53 +0100

    improved resulting df

--- 03_web_scraping/scrape_meteo.R
+++ 03_web_scraping/scrape_meteo.R
@@ -8,11 +8,10 @@ get_df_meteo <- function(month, year){
   }else{
     n_days <- day (as.Date(paste(year+1, 1, 1, sep="-")) - 1) 
   }
-  
   url = paste("http://www.ogimet.com/cgi-bin/gsynres?ord=REV&decoded=yes&ndays=",n_days, "&ano=", year, "&mes=", month, "&day=", n_days,"&hora=24&ind=08221", sep="")
   webpage <- read_html(url)
   data_table <- html_table(html_nodes(webpage, "table table")[[2]])
-  df <- data.frame(date = as.Date(data_table[-1,1], format="%d/%m/%Y"), hour= data_table[-1,2], "T(C)" = as.double(data_table[-1,3]), "ddd" = data_table[-1,7], "ff-kmh" = as.double(data_table[-1,8]), stringsAsFactors = FALSE) 
+    df <- data.frame(date = as.POSIXct(x = paste(data_table[-1,1],data_table[-1,2]), format= "%d/%m/%Y %H:%M", tz="GMT"), "T(C)" = as.double(data_table[-1,3]), "ddd" = data_table[-1,7], "ff-kmh" = as.double(data_table[-1,8]), stringsAsFactors = FALSE) 
 }
 
 list_df_meteo <- lapply(1:12, get_df_meteo, 2008 )
@@ -21,12 +20,16 @@ df_meteo <- do.call("rbind", list_df_meteo)
 
 colnames(df_meteo) <- c("date", "hour", "T(C)", "ddd", "ff kmh")
 
-sorted_df_meteo<- df_meteo[order(df_meteo[,1], df_meteo[,2]),]
+sorted_df_meteo<- df_meteo[order(df_meteo[,1]),]
 
-## We figure out the #6896 observation has missing values in column 3 and 5, that refers to T(C) and ff-kmh
+##We figure out the #6896 observation has missing values in column 3 and 5, that refers to T(C) and ff-kmh
 
 row_with_missing_values <- which(is.na(sorted_df_meteo), arr.ind=TRUE)
 
 ##Clean rows with missing values
 
 clean_df_meteo <- na.omit(sorted_df_meteo)
+
+##Summary of df
+
+summary(clean_df_meteo)
